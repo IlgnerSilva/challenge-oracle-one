1b5bde7fe10d0f112f5ffbc33526799de7a85773
Ilgner Silva Nascimento <ilgnersilva@outlook.com>
2022-12-20 13:07:13 -0300

    Finalizando responsivo

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const btnCripto = document.querySelector(".btn-cripto");
 const btnDescripto = document.querySelector(".btn-descripto");
-const textoDigitado = document.querySelector(".entrada-texto input");
-const msgErro = document.querySelector(".msg-erro")
-
+const textoDigitado = document.querySelector(".entrada-texto textarea");
+const msgErro = document.querySelector(".msg-erro");
+const displayNone = document.querySelectorAll(".display-none");
+const displayBlock = document.querySelectorAll(".display-block");
+const btnCopiar = document.querySelector(".copiar");
 //Regex para validar se as letras são minusculas e sem acentuação
 const regex = /^[a-z\s]+$/;
 
@@ -10,14 +12,17 @@ const regex = /^[a-z\s]+$/;
 function criptografar(){
     if(regex.test(textoDigitado.value)){
         const textoCripto = textoDigitado.value
-            .replaceAll('a', 'ai')
             .replaceAll('e', 'enter')
             .replaceAll('i', 'imes')
-            .replaceAll('o', 'ober')
+            .replaceAll('a', 'ai')
             .replaceAll('u', 'ufat')
+            .replaceAll('o', 'ober')
+
+        mudaDisplay();
         document.querySelector(".resultado textarea").innerHTML = textoCripto;
         msgErro.style.display = "none";
         textoDigitado.value = "";
+        btnCopiar.innerHTML = "Copiar texto";
     }else{
         msgErro.style.display = "block";
         msgErro.innerHTML = "Não foi possível criptografar este texto, verifique-o e tente novamente!";
@@ -28,19 +33,49 @@ function criptografar(){
 function descriptografar(){
     if(regex.test(textoDigitado.value)){
         const textoDescripto = textoDigitado.value
-            .replaceAll('ai', 'a')
             .replaceAll('enter', 'e')
             .replaceAll('imes', 'i')
-            .replaceAll('ober', 'o')
+            .replaceAll('ai', 'a')
             .replaceAll('ufat', 'u')
+            .replaceAll('ober', 'o')
+
+        mudaDisplay();
         document.querySelector(".resultado textarea").innerHTML = textoDescripto;
         msgErro.style.display = "none";
         textoDigitado.value = "";
+        btnCopiar.innerHTML = "Copiar texto";
     }else{
         msgErro.style.display = "block";
         msgErro.innerHTML = "Não foi possível descriptografar este texto, verifique-o e tente novamente!";
     }
 }
 
+function mudaDisplay(){
+    displayNone.forEach(elemento => {
+        //console.log(elemento.classList.replace("display-none", "display-block"))
+        console.log(elemento)
+        if(elemento.classList[1] === "none"){
+            elemento.classList.replace("display-none", "display-block")
+        }else{
+            elemento.classList.replace("display-none", "display-block")
+        }
+    })
+    displayBlock.forEach(elemento => {
+        //console.log(elemento.classList.replace("display-none", "display-block"))
+        console.log(elemento)
+        if(elemento.classList[1] === "block"){
+            elemento.classList.replace("display-block", "display-none")
+        }else{
+            elemento.classList.replace("display-block", "display-none")
+        }
+    })
+}
+
+function copiarTexto(){
+    navigator.clipboard.writeText(document.querySelector(".resultado textarea").value);
+    btnCopiar.innerHTML = "Texto copiado";
+}
+
+btnCopiar.addEventListener("click", copiarTexto)
 btnDescripto.addEventListener("click", descriptografar);
 btnCripto.addEventListener("click", criptografar);
